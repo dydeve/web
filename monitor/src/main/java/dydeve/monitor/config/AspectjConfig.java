6cc5a1e4fc7f8954e04ed4624591da0156f7580c
duyu <dydeve@163.com>
2017-07-30 23:19:47 +0800

    need log

--- monitor/src/main/java/dydeve/monitor/config/AspectjConfig.java
+++ monitor/src/main/java/dydeve/monitor/config/AspectjConfig.java
@@ -1,10 +1,12 @@
 package dydeve.monitor.config;
 
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by yuduy on 2017/7/29.
  */
+@Component
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class AspectjConfig {
 }
